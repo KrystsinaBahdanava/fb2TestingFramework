d0d34914582395dfa88d7c6787a7cbb157a63ef3
Krystsina Bahdanava <Krystsina_Bahdanava@epam.com>
2020-10-29 16:31:48 +0300

    Initial commit

---
+++ configurator.py
@@ -0,0 +1,2 @@
+class Configurator:
+    def init(self):
---
+++ main.py
@@ -0,0 +1,9 @@
+from configurator import Configurator
+
+
+def run():
+    config = Configurator(argv[1])
+
+
+if __name__ == '__main__':
+    run()
